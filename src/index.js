f58f132ae4dd864aaef9524800fdd8e55ed9c3d2
MooseCapital <48196495+MooseCapital@users.noreply.github.com>
2022-07-12 14:51:57 -0500

    save before testing svg files

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-console.log("hi");
+
 const tododetail = document.querySelector(".todo-detail");
 const todocheck = document.querySelector(".todo-check");
 const tododate = document.querySelector(".todo-date");
-
+const todotitle = document.querySelector(".todo-title");
 
 todocheck.addEventListener("click", () => {
     todocheck.classList.toggle("todo-check-active");
     tododetail.classList.toggle("todo-detail-active");
     tododate.classList.toggle("todo-date-active");
+    todotitle.classList.toggle("todo-title-active");
+
     console.log("works");
 })
 
