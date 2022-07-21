5126e654e8bfbaa7960ac3f6685b7a4926b61b23
MooseCapital <48196495+MooseCapital@users.noreply.github.com>
2022-07-21 04:51:50 -0500

    todo edit button removed

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
 
+
+
+const addproject = document.querySelector(".addproject");
+const add_project_overlay = document.querySelector(".add-project-overlay");
 const tododetail = document.querySelector(".todo-detail");
 const todocheck = document.querySelector(".todo-check");
 const tododate = document.querySelector(".todo-date");
 const todotitle = document.querySelector(".todo-title");
+const add_project_close = document.querySelector(".add-project-close");
 
+
+
+addproject.addEventListener("click", () => {
+    add_project_overlay.style.display = "grid";
+    console.log(add_project_overlay.style.display);
+})
+add_project_close.addEventListener("click" ,() => {
+    add_project_overlay.style.display = "none";
+    console.log(add_project_overlay.style.display);
+})
 todocheck.addEventListener("click", () => {
     todocheck.classList.toggle("todo-check-active");
     tododetail.classList.toggle("todo-detail-active");
@@ -12,8 +27,6 @@ todocheck.addEventListener("click", () => {
 
     console.log("works");
 })
-
-
 
 
 
