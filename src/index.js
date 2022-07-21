2a0e006ed26b0f4f644844c412964e85599ae4e4
MooseCapital <48196495+MooseCapital@users.noreply.github.com>
2022-07-21 10:42:24 -0500

    priority button added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import {createToDo} from "./dom";
 
-
+createToDo("title ex.", "high", "june 3");
 const addproject = document.querySelector(".addproject");
 const add_project_overlay = document.querySelector(".add-project-overlay");
 const tododetail = document.querySelector(".todo-detail");
@@ -14,6 +14,15 @@ const sidebar_project = document.querySelector(".add-project-sidebar-project");
 const todo_main = document.querySelector(".add-project-main");
 const project_main = document.querySelector(".create-project-main");
 
+const low_btn = document.querySelector(".create-priority-btn-low");
+const med_btn = document.querySelector(".create-priority-btn-medium");
+const high_btn = document.querySelector(".create-priority-btn-high");
+const lowinput = document.querySelector("#create-priority-low");
+const medinput = document.querySelector("#create-priority-medium");
+const highinput = document.querySelector("#create-priority-high");
+
+
+
 addproject.addEventListener("click", () => {
     add_project_overlay.style.display = "grid";
     console.log(add_project_overlay.style.display);
@@ -22,6 +31,7 @@ add_project_close.addEventListener("click" ,() => {
     add_project_overlay.style.display = "none";
     console.log(add_project_overlay.style.display);
 })
+// turn into queryselector all and use e.target, have to convert all to add classlist on specific todo divs
 todocheck.addEventListener("click", () => {
     todocheck.classList.toggle("todo-check-active");
     tododetail.classList.toggle("todo-detail-active");
@@ -48,7 +58,7 @@ sidebar_todo.addEventListener("click", () => {
     if (project_main.style.display === "grid") {
         project_main.style.display = "none";
     }
-    console.log("test todo")
+
 })
 
 sidebar_project.addEventListener("click", () => {
@@ -67,12 +77,26 @@ sidebar_project.addEventListener("click", () => {
     if (todo_main.style.display === "grid") {
         todo_main.style.display = "none";
     }
-    console.log("test project")
+
 })
 
+lowinput.onclick = () => {
+    low_btn.classList.toggle("priority-btn-low-active");
+    med_btn.classList.remove("priority-btn-medium-active");
+    high_btn.classList.remove("priority-btn-high-active");
+}
 
+medinput.onclick = () => {
+    med_btn.classList.toggle("priority-btn-medium-active")
+    low_btn.classList.remove("priority-btn-low-active");
+    high_btn.classList.remove("priority-btn-high-active");
+}
 
-
+highinput.onclick = () => {
+    high_btn.classList.toggle("priority-btn-high-active");
+    low_btn.classList.remove("priority-btn-low-active");
+    med_btn.classList.remove("priority-btn-medium-active");
+}
 
 
 
